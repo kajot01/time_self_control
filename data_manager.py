ccf0f1eeafed33b99c84e4e21526501873fa765d
Jacek Lipiński <01158788@pw.edu.pl>
2023-12-07 00:32:17 +0100

    Dopisane funkcji start_timer i stop_timer Te funkcje będzie można zaimplementować bezpośrednio do przycisków w gui

--- data_manager.py
+++ data_manager.py
@@ -4,22 +4,22 @@ from datetime import datetime
 
 
 def get_time():
-    return datetime.now()
+    H = int(datetime.now().strftime("%H"))
+    m = int(datetime.now().strftime("%M"))
+    return [H, m]
 
 
 def get_date():
-    year = datetime.now().strftime("%Y")
-    month = datetime.now().strftime("%m")
-    day = datetime.now().strftime("%d")
-    return [year, month, day]
+    return datetime.now().strftime("%Y-%m-%d")
+
 
 class DataManager:
-    def __init__(self, data_b):
-        self.data_b = data_b
+    def __init__(self, sql_conn):
+        self.sql_conn = sql_conn
         # [Y, m, d]
-        self.date = 0
+        self.date = None
         # [H, M]
-        self.timer_start = 0
+        self.timer_start = None
         self.is_productive = True
         self.conn = SQLConn(3306, "JML", "cSJH9xB+^SvV", "time_counting")
 
@@ -29,8 +29,21 @@ class DataManager:
         else:
             self.is_productive = False
 
-        self.timer_start = get_time()
+        self.stop_timer()
         self.date = get_date()
+        self.timer_start = get_time()
 
-    # def save_entry(self):
-
+    def stop_timer(self):
+        if self.timer_start is not None:
+            d = lambda t1, t2: (t2[0] - t1[0]) * 60 + t2[1] - t1[1]
+            if self.date == get_date():
+                tn = get_time()
+                self.sql_conn.make_date_record(self.date, self.is_productive, self.timer_start, tn,
+                                               d(self.timer_start, tn))
+            else:
+                self.sql_conn.make_date_record(self.date, self.is_productive, self.timer_start, "23:59:00",
+                                               d(self.timer_start, [23, 59]))
+                tn = get_time()
+                self.sql_conn.make_date_record(get_date(), self.is_productive, "00:00:00", tn,
+                                               d([0, 0], tn))
+            self.timer_start = None
